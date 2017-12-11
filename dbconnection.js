fd7350ba4c3b0a5d3f2892915ae0022614697a34
ratish <r_retnakaran@infosys.com>
2017-12-11 13:55:31 +0530

    Initial Commit

---
+++ dbconnection.js
@@ -0,0 +1,62 @@
+//var mongoURL = 'mongodb://localhost:27017/test';
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+var db = null;
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
+}
+
+//DATABASE 
+module.exports = {
+    initDb: function(callback) {
+
+        console.log('initDb');  
+        console.log('mongoURL', mongoURL); 
+      
+      if (mongoURL == null) return;
+
+      var mongodb = require('mongodb');
+      if (mongodb == null) return;
+
+      mongodb.connect(mongoURL, function(err, conn) {
+        if (err) {
+          callback(err);
+          console.log('initDb Error connecting');        
+          return;
+        }
+
+        db = conn;
+
+        console.log('Connected to MongoDB at: %s', mongoURL);
+      });
+  },
+    
+    getDB:function(){
+        if (!db) {
+            console.log('getDB db is NULL #############');        
+          initDb(function(err){
+              console.log('Error connecting to Mongo. Message:\n'+err);
+          });
+          return db;
+        }
+        else {
+            return db;
+        }
+    }
+};
---
+++ routes/mediaservice.js
@@ -0,0 +1,95 @@
+//Media Services APIS' a exposed in here
+var express = require('express')
+var router = express.Router()
+
+var persistObj = require('../dbconnection.js')
+var imdbModule = require('./imdbModule.js')
+
+//variants for movie json
+var d = new Date()
+var currdate;
+var movieDetailsJson = {};
+var curr_date = d.getDate();
+var curr_month = d.getMonth() + 1; //Months are zero based
+var curr_year = d.getFullYear();
+currdate = curr_year+ "-" + curr_month + "-" + curr_date;
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+//ACTORS
+//GET ACTORS
+router.get('/actors', function (req, res) {
+    dbObj = persistObj.getDB()        
+    if (dbObj)
+    {
+        dbObj.collection('actors').aggregate( ).toArray(function(err, result){
+            console.log("printing result: ", result);  
+            res.send(result);     
+        });  
+    } 
+    else
+    {   
+        console.log("dbObj is INVALID");  
+    }
+});
+
+
+//ADD an ACTOR
+router.put('/actor', function (req, res) {
+    console.log("PUT request Recieved for Actor")
+    console.log("actor info to be added", req.body);   
+    
+    dbObj = persistObj.getDB()        
+    if (dbObj)
+    {
+        console.log("Saving the JSON Body")
+        dbObj.collection('actors').save(req.body, function(err, records){
+            if (err) throw err;
+            console.log("record added");
+        });
+    }
+    else
+    {
+        console.log("NO DB FOUND");
+    }
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
+
+//DELETE ACTOR BY ID
+router.delete('/actor', function (req, res) {
+    console.log("DELETE ACTOR request Recieved")
+    console.log("actor id to be deleted: ", req.query.id);   
+    dbObj = persistObj.getDB()        
+    if (dbObj)
+    {
+        console.log("Deleting the entry")
+        var ObjectId = require('mongodb').ObjectID;
+        dbObj.collection('actors').deleteMany(
+            {"_id":ObjectId(req.query.id)}, function(err, results){
+            if (err) throw err;
+            console.log("record deleted");
+            res.send(results);
+        });
+    }
+    else
+    {
+        console.log("NO DB FOUND");
+    }
+});
+
+router.get('/latestMovies',function(req, res){
+    imdbModule.getLatestMovies(currdate,function(movieResults){
+        console.log("latest Movies");
+        moviesResponse = {results:movieResults}    
+        res.json(moviesResponse);
+    });
+    console.log("latest Movies service end of block");    
+});
+
+module.exports = router;
