3f5a95b577361388f46ced8ae8f089c983edc7e2
ratish <c-ratish.retnakaran@charter.com>
2018-01-24 12:13:17 +0530

    Add response headers to support request from reactjs or other node js servers.

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(express.static('public'));  
 
 //ROUTE MEDIA SERVICE CALL: example: http://127.0.0.1:8888/mediaservice/actors
+app.use(function (req, res, next) {
+    
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/mediaservice', mediaservice)
 
 
